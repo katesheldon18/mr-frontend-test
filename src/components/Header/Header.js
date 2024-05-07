4075a96ca7f5d26ea7672f24f824a5e468535819
Kate <katshe18@gmail.com>
2024-05-07 18:50:31 +1200

    Started work on media breakpoints / 2-column layout for large devices. Added cart icon.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,10 @@
 import styles from "./Header.module.scss";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 export const Header = ({ cart }) => {
     return (
         <div className={styles.container}>
+            <ShoppingCartIcon style={{ color: '#888888', fontSize: '24px' }} />
             <div className={styles.count}>( {cart.length} )</div>
         </div>
     )
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -12,31 +12,35 @@ export const Product = ({
 }) => {
     return (
         <div className={styles.container}>
-            <img className={styles.image} src={imageURL} alt={title} />
-            <div className={styles.title}>{title}</div>
-            <div className={styles.price}>${price}.00</div>
-            <div className={styles.description}>{description}</div>
-            <div className={styles["size-heading"]}>
-                SIZE<span className={styles.required}>*</span>
-                <div className={styles.letter}>{size}</div>
+            <div className={styles["column-1"]}>
+                <img className={styles.image} src={imageURL} alt={title} />
             </div>
-            <div className={styles["size-container"]}>
-                {sizeOptions.map((sizeOption) => {
-                    const isSelected = size === sizeOption.label;
-                    return (
-                        <div
-                            className={cx(
-                                styles.sizes,
-                                isSelected && styles["sizes--selected"]
-                            )}
-                            onClick={() => {
-                                onSizeClick(sizeOption.label);
-                            }}
-                        >
-                            {sizeOption.label}
-                        </div>
-                    );
-                })}
+            <div className={styles["column-2"]}>
+                <div className={styles.title}>{title}</div>
+                <div className={styles.price}>${price}.00</div>
+                <div className={styles.description}>{description}</div>
+                <div className={styles["size-heading"]}>
+                    SIZE<span className={styles.required}>*</span>
+                    <div className={styles.letter}>{size}</div>
+                </div>
+                <div className={styles["size-container"]}>
+                    {sizeOptions.map((sizeOption) => {
+                        const isSelected = size === sizeOption.label;
+                        return (
+                            <div
+                                className={cx(
+                                    styles.sizes,
+                                    isSelected && styles["sizes--selected"]
+                                )}
+                                onClick={() => {
+                                    onSizeClick(sizeOption.label);
+                                }}
+                            >
+                                {sizeOption.label}
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
         </div>
     );
